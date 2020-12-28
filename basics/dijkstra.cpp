50fc42703ddd750d7a1750924f1961785598deca
Jason Hou <darkstar.hou2@gmail.com>
2020-12-28 01:07:14 -0500

    dijkstra c++

--- basics/dijkstra.cpp
+++ basics/dijkstra.cpp
@@ -1,20 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
-int N, M, a, b, c, curr, dist[200000];
+int N, M, a, b, c, dist[200000];
 vector<vector<int>> adj[200000];
+vector<int> curr;
 const int inf = 0x3f3f3f3f;
 
+struct Cmp {
+    bool operator()(vector<int>&a, vector<int>&b){ return a[1] < b[1];}
+};
 
 void dijkstra(int start){
-  priority_queue<int> q; q.push(start);
+  priority_queue<vector<int>, vector<vector<int>>, Cmp> q;
+  q.push({start, 0});
   dist[start] = 0;
 
   while(!q.empty()){
     curr = q.top(); q.pop();
-    for (auto node: adj[curr]){
-      if (dist[curr] + node[1] < dist[node[0]]){
-        dist[node[0]] = dist[curr] + node[1];
-        q.push(node[0]);
+    for (auto node: adj[curr[0]]){
+      if (curr[1] + node[1] < dist[node[0]]){
+        dist[node[0]] = curr[1] + node[1];
+        q.push({node[0], dist[node[0]]});
       }
     }
   }
