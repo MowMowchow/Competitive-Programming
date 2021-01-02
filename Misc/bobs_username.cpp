0804e14f967fa49b2fc578edda62e4334f28a473
Jason Hou <darkstar.hou2@gmail.com>
2021-01-02 17:08:18 -0500

    initial commit

--- Misc/bobs_username.cpp
+++ Misc/bobs_username.cpp
@@ -1,9 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 const int inf = 0x3f3f3f3f;
-
+long long N, K, ans = 1;
 
 int main(){
+  cin >> K >> N;
 
+  for (int i = 0; i < K; i++){
+    ans *= N;
+  }
+  cout << ans << "\n";
   return 0;
 }
