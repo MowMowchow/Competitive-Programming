38e056e63390ffb1cd31405a3af9a22241426e51
Jason Hou <darkstar.hou2@gmail.com>
2021-01-03 16:41:30 -0500

    initial commit

--- Misc/bobs_splitting_a_sequence.cpp
+++ Misc/bobs_splitting_a_sequence.cpp
@@ -1,18 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 const int inf = 0x3f3f3f3f;
-int l, r, N, arr[300010], s1, s2, s3;
+long long l, r, N, arr[300010], low, high, finall = inf;
 
-// Nlogn
-// fix middle block and use that to binary search
-//  condition is to minimize abs(leftsum-rightsum)
+
 int main(){
-  cin >> N >> arr[0];
-  for (int i = 1; i < N; i++){
+  memset(arr, 0, sizeof(arr));
+  cin >> N;
+  for (int i = 1; i <= N; i++){
     cin >> arr[i];
     arr[i] += arr[i-1];
   }
-
-  
+  l = 2; r = 2;
+  while (l <= r){
+    low = min(abs(arr[N]-arr[r]), min(abs(arr[r]-arr[l-1]), abs(arr[l-1]-arr[0])));
+    high = max(abs(arr[N]-arr[r]), max(abs(arr[r]-arr[l-1]), abs(arr[l-1]-arr[0])));
+    finall = min(finall, abs(high-low));
+    if (abs(arr[r]-arr[l-1]) <= abs(arr[N]-arr[r])){ // moving r up
+      r++;
+      if (r == N){
+        break;
+      }
+    }
+    else {
+      l++;
+    } 
+  }
+  cout << finall << "\n";
   return 0;
 }
